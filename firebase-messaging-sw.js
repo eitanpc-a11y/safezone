9012083c1da2e7029b1cbc926f0bc3f88aa7be84
eitanpc-a11y <eitanpc@gmail.com>
2026-03-15 11:12:02 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,9 @@
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
+// מפתחות מעודכנים וזהים ל-index.html
 firebase.initializeApp({
-  apiKey: "AIzaSyDLKF30weC2z70WvK3zvDoOqIVWYfoQ9ow",
+  apiKey: "gyKZFrNtLB3we67OY8_iohg6h9TtmRBlpMP1bj1DY_k",
   projectId: "safezone-cd4b5",
   messagingSenderId: "78949340231",
   appId: "1:78949340231:web:85929f70752f1fc7cf8d62"
@@ -11,12 +12,13 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
+  console.log('הודעה התקבלה ברקע:', payload);
   const notificationTitle = payload.notification.title || "SafeZone Alert";
   const notificationOptions = {
     body: payload.notification.body,
     icon: 'https://cdn-icons-png.flaticon.com/512/564/564619.png',
     vibrate: [500, 100, 500],
-    data: { url: './index.html?alert=true' }
+    data: { url: './index.html' }
   };
   return self.registration.showNotification(notificationTitle, notificationOptions);
 });
